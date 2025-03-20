cde0f531926e5127a3e4d6fe6230d33fc76034d5
CPC67128 <steve.fuchs@free.fr>
2025-03-20 21:49:58 +0100

    check disqus

--- Build_Production.bat
+++ Build_Production.bat
@@ -1,4 +1,3 @@
 cd C:\Repositories\JekyllWebSite
-explorer C:\Repositories\JekyllWebSite\_site
 set JEKYLL_ENV=production
-"C:\Ruby26-x64\bin\bundle.bat" exec jekyll build
+"C:\Ruby33-x64\bin\bundle.bat" exec jekyll build
